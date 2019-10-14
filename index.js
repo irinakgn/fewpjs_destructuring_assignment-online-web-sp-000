a859e0331b7810ae8fa55696d0a02b6d1af7eccb
Irina Kagan <irinagamb@gmail.com>
2019-10-14 19:45:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ console.log(red, orange, yellow, green, blue, indigo, violet)
 const [r, o, y, g, b, , v] = colors
 console.log(r, o, y, g, b,v)
 
-const [, , , , , ingd, ]= colors
-console.log(ingd)
+const [, , , , , indg, ]= colors
+console.log(indg)
 
 const muppet = {
   name: 'Kermit',
