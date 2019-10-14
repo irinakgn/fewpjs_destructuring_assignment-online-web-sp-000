49650a2774f33782f1312f4e1c0c008e2d67d15b
Irina Kagan <irinagamb@gmail.com>
2019-10-14 20:02:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const k_muppet = {
 const { song_2, song_4} = muppet.album.the_muppet_movie;
 console.log(song_2);
 console.log(song_4);
-const {color, k_job, k_partnet}= muppet;
+const { color, k_job, k_partnet}= muppet;
 console.log(color);
 console.log(k_job);
 console.log(k_partner);
