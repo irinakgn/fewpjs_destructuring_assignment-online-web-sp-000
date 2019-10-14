fb1cfebac118a44db36a7ed88576ad2e90ba01b3
Irina Kagan <irinagamb@gmail.com>
2019-10-14 19:58:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const farmAnimals = ['cow', 'horse', 'sheep', 'pig', 'chicken']
 const sounds = ['moo', 'nai', 'ba', 'oink', 'cluck']
-const [moo, neigh, ba, oink, cluck] = farmAnimals
-console.log(moo, neigh, ba, oink, cluck)
+const [moo, neigh, baa, oink, cluck] = farmAnimals
+console.log(moo, neigh, baa, oink, cluck)
 
 
 const [Bessie, , Dolly, Babe, Little] = farmAnimals
